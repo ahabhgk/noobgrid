2f0d56895c43a6e4b96e60b2cb751a2e15cea7f5
ahabhgk <ahabhgk@gmail.com>
2019-04-19 18:10:31 +0800

    :star2:finish.

--- src/js/index.js
+++ src/js/index.js
@@ -3,24 +3,39 @@ const drop = document.querySelector('.drop')
 const barLinksWrap = document.querySelector('.bar-links-wrap')
 const logo = document.querySelector('.logo')
 const navMain = document.querySelector('.nav-main')
-const linkHeight = document.querySelector('.bar-links').offsetHeight
 const navHeight = document.querySelector('.nav-main>div').offsetHeight * 2
-const linkNum = document.querySelectorAll('.bar-links').length
-const navNum = document.querySelectorAll('.nav-main a').length
+const navLength = document.querySelectorAll('.nav-main a').length * navHeight
 
-function dropper(height, it, num) {
+function dropper(direction, length, it) {
   let isHidden = true
   function dropIt() {
     if (isHidden) {
-      it.style.height = `${height * num}px`
+      it.style[direction] = length
       isHidden = false
     } else {
-      it.style.height = '0px'
+      it.style[direction] = '0px'
       isHidden = true
     }
   }
   return dropIt
 }
 
-drop.addEventListener('touchstart', dropper(linkHeight, barLinksWrap, linkNum))
-logo.addEventListener('touchstart', dropper(navHeight, navMain, navNum))
+if (window.screen.width <= 414) {
+  logo.addEventListener('touchstart', dropper('height', `${navLength}px`, navMain))
+}
+drop.addEventListener('touchstart', dropper('width', '70vw', barLinksWrap))
+
+function stopMove() {
+  let isMovable = true
+  return function () {
+    if (isMovable === true) {
+      document.body.style.overflow = 'hidden'
+      isMovable = false
+    } else {
+      document.body.style.overflow = 'auto'
+      isMovable = true
+    }
+  }
+}
+
+drop.addEventListener('touchstart', stopMove())
